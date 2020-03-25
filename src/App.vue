ccdbf842514b55c76d4dafe53e404c0d2058841b
Matthew Morek <matthew@madbit.co>
2020-03-25 22:14:49 +0000

    💅 Animate page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <transition>
+  <transition name="scale">
     <router-view />
   </transition>
 </template>
+
+<style lang="css">
+.scale-enter-active {
+  animation: fade-in-fwd 0.6s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+}
+
+@keyframes fade-in-fwd {
+  0% {
+    transform: translateZ(-80px);
+    opacity: 0;
+  }
+  100% {
+    transform: translateZ(0);
+    opacity: 1;
+  }
+}
+</style>
